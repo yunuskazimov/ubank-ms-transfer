56306a3c7044dffc83575a031c8c5628094dbe45
Yunus Kazimov <yunus.kazimov@a2z.az>
2023-12-20 20:56:05 +0400

    Add existing project files to Git

---
+++ src/main/java/az/ubank/mstransfer/MsTransferApplication.java
@@ -0,0 +1,13 @@
+package az.ubank.mstransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTransferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ubank/mstransfer/MsTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ubank.mstransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
